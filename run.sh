adb486d9386c3f3865aaeb60c03bc8f02e175ed2
Marius Preyers <marius.preyers@aoe.com>
2020-07-15 14:55:24 +0200

    Add basic ansible config

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Find script directory
+SCRIPT="$0"
+if [ ! -e "$SCRIPT" ]; then
+  case "$SCRIPT" in
+    (*/*)
+      exit 1
+      ;;
+    (*)
+      SCRIPT=$(command -v -- "$SCRIPT") || exit 1
+      ;;
+  esac
+fi
+SCRIPT_DIR=$(cd -P -- "$(dirname -- "$SCRIPT")" && pwd -P) || exit 1
+
+# Run ansible
+sudo PLAYBOOK_DIR="$SCRIPT_DIR" ansible-playbook -i hosts "$@"
